75d96201c431152fbd0f721ee02da80ce953edd1
Kaidong Wang <wkd20040211@gmail.com>
2025-12-18 14:50:14 +0800

    这是一个新的项目

---
+++ progect2App.swift
@@ -0,0 +1,17 @@
+//
+//  progect2App.swift
+//  progect2
+//
+//  Created by Kaidong Wang on 12/18/25.
+//
+
+import SwiftUI
+
+@main
+struct progect2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
